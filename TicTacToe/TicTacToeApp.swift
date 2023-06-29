0749837789e5e08285bc7a7ecf52f775be9d9dcf
mel <melissa.sargent@hotmail.co.uk>
2023-06-29 21:08:30 +0100

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Melissa Sargent on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
