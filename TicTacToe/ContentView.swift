bfa8eeacdf509692e4f84a17126cb0c4dc5a667c
mel <melissa.sargent@hotmail.co.uk>
2023-07-11 20:39:25 +0100

    Basic structure

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -7,15 +7,53 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct ContentView: View
+{
+    @StateObject var gameState = Gamestate()
+    
+    var body: some View
+    {
+        let borderSize = CGFloat(5)
+        
+        VStack(spacing: borderSize)
+        {
+            ForEach(0...2, id: \.self)
+            {
+                row in
+                HStack(spacing: borderSize)
+                {
+                    ForEach(0...2, id: \.self)
+                    {
+                        column in
+                        
+                        let cell = gameState.board[row][column]
+                        
+                        Text(cell.displayTile())
+                            .font(.system(size: 60))
+                            .foregroundColor(cell.tileColor())
+                            .bold()
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .aspectRatio(1, contentMode: .fit)
+                            .background(Color.white)
+                            .onTapGesture {
+                                gameState.placeTile(row, column)
+                            }
+                    }
+                }
+            }
         }
+        .background(Color.black)
         .padding()
+        .alert(isPresented: $gameState.showAlert)
+        {
+            Alert(
+                title: Text(gameState.alertMessage),
+                dismissButton: .default(Text("Okay"))
+                {
+                    gameState.resetBoard()
+                }
+            )
+        }
     }
 }
 
